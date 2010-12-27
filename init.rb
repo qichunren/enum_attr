b235ff8d2ec7e0775f6b3f6be63e5691934832c4
qichunren <whyruby@gmail.com>
2010-12-27 15:23:47 +0800

    add a new method into enum

--- init.rb
+++ init.rb
@@ -1,2 +1,5 @@
-require 'enum_attr'
+Bundler.require(:default)
+
+require File.join(File.dirname(__FILE__), "lib", "enum_attr") 
+
 Object.send :include, EnumAttr::Mixin
--- lib/enum_attr.rb
+++ lib/enum_attr.rb
@@ -23,17 +23,29 @@ module EnumAttr
     # end
     #
     def enum_attr(attr, enums)
+      raise "The secone param must be a array!" unless enums.is_a? Array
+      
       attr = attr.to_s
+      
       enums.each do |enum|
         const_set("#{attr.upcase}_#{enum[2].upcase}", enum[1] )
+        
+        # TODO: how to judge if there is active_record 3 gem here?
         scope "#{attr}_#{enum[2]}".to_sym, where("#{attr}=#{enum[1]}") 
         
         # TODO define_method such as "stauts_origin?"
-      end
+        define_method "#{attr}_#{enum}?" do
+          attr == enum[1] 
+        end 
+        
+      end # end: enums.each
+      
       self.class_eval(%Q{
 
 ENUMS_#{attr.upcase} = enums.collect{|item| [item[0], item[1]]}
+
 validates_inclusion_of attr, :in => enums.map{|e| e[1].to_i}, :allow_nil => true
+
 def #{attr}_name
   ENUMS_#{attr.upcase}.find{|option| option[1] == #{attr}}[0] unless #{attr}.nil?
 end
